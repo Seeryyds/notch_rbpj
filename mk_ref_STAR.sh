749c38d407cc6b4ca7682a7448feb8538c60ebe8
Seeryyds <158558757+Seeryyds@users.noreply.github.com>
2026-03-01 19:26:29 +0100

    Add script to generate STAR genome index for GRCm39

---
+++ mk_ref_STAR.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -J mkSTAR_GRCm39_109
+#SBATCH -t 08:00:00
+#SBATCH --cpus-per-task=18
+#SBATCH --mem=80G
+#SBATCH -o ~/data_storage/working_data/notch_rbpj/logs/%x_%j.out
+#SBATCH -e ~/data_storage/working_data/notch_rbpj/logs/%x_%j.err
+
+set -euo pipefail
+
+# STAR 
+STAR --version
+
+VERSION=109
+BASE=~/data_storage/working_data/notch_rbpj/refs
+gtf_file=${BASE}/Mus_musculus.GRCm39.${VERSION}.gtf
+fasta_file=${BASE}/Mus_musculus.GRCm39.dna.toplevel.fa
+ref_name=${BASE}/star_index_GRCm39_${VERSION}
+
+mkdir -p "$ref_name"
+ls -lh "$gtf_file" "$fasta_file"
+
+STAR --runMode genomeGenerate \
+  --runThreadN "${SLURM_CPUS_PER_TASK}" \
+  --genomeDir "$ref_name" \
+  --genomeFastaFiles "$fasta_file" \
+  --sjdbGTFfile "$gtf_file" \
+  --sjdbOverhang 99
